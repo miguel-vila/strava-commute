a6988aa5d30f007565371b70adeb0e5ec09a4ec9
miguel <miguelvilag@gmail.com>
2016-04-18 00:13:03 -0500

    algunos avances

--- es6/main.js
+++ es6/main.js
@@ -1,4 +1,7 @@
+'use strict';
+
 import axios from 'axios';
+import queryString from 'query-string';
 
 const client_id = "2957";
 const redirect_uri = "http://localhost:5000";
@@ -7,6 +10,18 @@ const response_type = "code";
 const stravaButton = document.getElementById('strava-button');
 const authUrl = `https://www.strava.com/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}`;
 stravaButton.href = authUrl;
+
+const queryParams = queryString.parse(location.search);
+
+const code = queryParams.code;
+if(code) {
+  axios.get('my-commutes', { params : { code } } ).then( (response) =>
+    console.log(response),
+  (err) =>
+    console.log(err)
+  )
+}
+
 /*
 axios.get('http://jsonplaceholder.typicode.com/posts').then(function (resp) {
   console.log(resp)
--- web.js
+++ web.js
@@ -1,28 +1,93 @@
-var fs = require('fs');
-var express = require('express');
-var strava = require('strava-v3');
-var promisify = require("promisify-node");
-var bodyParser = require('body-parser');
-var getToken = promisify(strava.oauth.getToken);
-var getActivities = promisify(strava.athlete.listActivities);
+'use strict';
+
+const fs         = require('fs');
+const express    = require('express');
+const strava     = require('strava-v3');
+const promisify  = require("es6-promisify");
+const bodyParser = require('body-parser');
+const q          = require('q');
+const lazy       = require('lazy.js');
+const moment     = require('moment');
+
+const getToken             = promisify(strava.oauth.getToken);
+const getAthleteActivities = (access_token) => {
+  let deferred = q.defer();
+  strava.athlete.listActivities({access_token, per_page: 15}, (error, activities) => {
+    if(error) {
+      deferred.reject(error);
+    } else {
+      deferred.resolve(activities);
+    }
+  });
+  return deferred.promise;
+}
+
+const normalizeActivity = (activity) => {
+  let start_date = moment.parseZone(activity.start_date_local);
+  let end_date = start_date.add( activity.elapsed_time, 'seconds' );
+  let date = start_date.format('DD/MM/YYYY');
+  let normalized = {
+    id: activity.id,
+    distance: activity.distance,
+    moving_time: activity.moving_time,
+    start_date,
+    end_date,
+    date,
+    start: activity.start_latlng,
+    end: activity.end_latlng,
+    achievement_count: activity.achievement_count,
+    average_speed: activity.average_speed,
+    max_speed: activity.max_speed,
+    average_watts: activity.average_watts,
+    kilojoules: activity.kilojoules
+  };
+  return normalized;
+}
+
+const getCommutes = (activities) =>
+  lazy(activities)
+  .filter( (activity) => activity.commute )
+  .map(normalizeActivity)
+  .groupBy('date')
+  .filter( (activities) => activities.length === 2 ) // filtrar 2 actividades por día
+  /*
+  .map( (activities) => {
+    console.log('activities = ', activities);
+    return activities.sort( (a1,a2) => {
+      let duration = moment.duration(a1.start_date.diff(a2.start_date));
+      let h = duration.asHours();
+      console.log('diff = ',h)
+      return h
+    })
+  })
+  */
+  .toArray()
+
+const getAthleteAndActivities = (auth_code) =>
+  getToken(auth_code).then( (response) =>
+    getAthleteActivities(response.access_token).then( (activities) =>
+      ({ activities , athlete : response.athlete })
+    )
+ )
 
 var app = express();
 app.use(bodyParser.json());
 
-app.post('/my-commutes', function (req, res) {
-  var code = req.body && req.body.code;
+app.get('/my-commutes', (req, res) => {
+  var code = req.query && req.query.code;
   if(!code) {
     res.status(404)
-       .json({ msg: 'Must send json with "code" field' })
+       .json({ msg: 'Must send query parameter "code"' })
   } else {
-    getToken(code).then(function(response){
-      console.log(response);
-    });
+    getAthleteAndActivities( code ).then( (response) => {
+      console.log(getCommutes(response.activities));
+//      console.log(response.athlete);
+    }, (err) => console.log(err));
     res.json({ok:true});
   }
 });
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   var path = __dirname + '/index.html';
   var stat = fs.statSync(path);
   res.setHeader('Content-Type', 'text/html; charset=UTF-8');
@@ -36,6 +101,4 @@ app.use('/dist', express.static(__dirname + '/dist'));
 app.use('/img', express.static(__dirname + '/img'));
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(port, () => console.log("Listening on " + port) );
